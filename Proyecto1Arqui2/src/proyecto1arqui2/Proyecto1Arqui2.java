417b9cc0dc179ef9c8bef4626ae6195c5482fb16
Armando <armandofg3937@gmail.com>
2023-03-12 17:08:42 -0600

    Generador Creado

---
+++ Proyecto1Arqui2/src/proyecto1arqui2/Proyecto1Arqui2.java
@@ -0,0 +1,23 @@
+
+package proyecto1arqui2;
+
+/**
+ *
+ * @author arman
+ */
+public class Proyecto1Arqui2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Interfaz ui;
+        ui = new Interfaz();
+        ui.setVisible(true);
+      
+        
+        
+    }
+    
+}
---
+++ Proyecto1Arqui2/src/proyecto1arqui2/Random.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto1arqui2;
+
+/**
+ *
+ * @author arman
+ */
+public class Random {
+    
+    private double semilla=12345.6789;
+    
+    public Boolean Bernoulli(double p){
+        
+        double ri = generarAleatorio();
+        
+        if (ri <= p){
+            return true;
+        }else{
+            return false;
+        }
+        
+        
+    }
+    
+    
+    public double generarAleatorio() {
+        
+        double a = 16807;
+        double m = (2^31)-1;
+        
+        double aleatorio=0;
+       
+        //System.out.println("Semilla: " + semilla);
+        aleatorio = (a * semilla) % m;
+        aleatorio=aleatorio/m;
+        
+        semilla = aleatorio; // Actualizar la semilla para la siguiente iteración
+
+        
+        //System.out.println("Aleatorio: " + aleatorio);
+        
+        return aleatorio;
+        
+    }
+    
+    
+    
+}
